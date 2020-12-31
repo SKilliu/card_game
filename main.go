3138d364832f19b1dfafd7f651d1775d773561eb
skiliushyk <skiliushyk@gmail.com>
2020-12-31 13:29:33 +0200

    Added simple example of tamplete generating

--- main.go
+++ main.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/SKilliu/card_game/models"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Card game API started"))
+		file, err := os.Open("./static/html/page.html")
+		if err != nil {
+			return
+		}
+		defer file.Close()
+
+		t, err := template.ParseFiles("./static/html/page.html")
+		if err != nil {
+			return
+		}
+
+		//t, err := template.New("webpage").Parse(tmpl)
+		//check(err)
+
+		data := struct {
+			Title string
+			Items []string
+		}{
+			Title: "My page",
+			Items: []string{
+				"My photos",
+				"My blog",
+			},
+		}
+
+		err = t.Execute(w, data)
+		if err != nil {
+			return
+		}
 	})
 
 	game := models.NewGame(2, "x1")
